6f84894e63a638f9b5d6e49f752e9f334d7eb274
upbook26 <upbook.26@gmail.com>
2020-12-08 23:54:14 +0900

    12/8 update

---
+++ resources/views/generate_pdf.blade.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <style type="text/css">
+        @font-face {
+            font-family: ipag;
+            font-style: normal;
+            font-weight: normal;
+            src: url('{{ storage_path('fonts/ipag.ttf') }}') format('truetype');
+        }
+        @font-face {
+            font-family: ipag;
+            font-style: bold;
+            font-weight: bold;
+            src: url('{{ storage_path('fonts/ipag.ttf') }}') format('truetype');
+        }
+        body {
+            font-family: ipag !important;
+        }
+    </style>
+</head>
+<body>
+<div>
+    <table>
+        <tr>
+            <td style="width:50%;">
+                <div style="padding:15px;">
+                    テストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテスト
+                </div>
+            </td>
+            <td>test</td>
+            <td>test</td>
+        </tr>
+        <tr>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+        </tr>
+        <tr>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+        </tr>
+    </table>
+</div>
+
+</body>
+</html>
